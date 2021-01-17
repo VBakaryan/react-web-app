d9a4b3f62d7435dbadedbe0166853ee7e26ef56f
Your Name <you@example.com>
2021-01-18 00:58:39 +0400

    fix asset floder

--- src/data-mockup/products-mockup.js
+++ src/data-mockup/products-mockup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import BlockImg1 from '../assets/premium.67475998.png'
-import BlockImg2 from '../assets/regular-plus.0fcb7d0c.png'
+import BlockImg1 from '../assets/products/premium.67475998.png'
+import BlockImg2 from '../assets/products/regular-plus.0fcb7d0c.png'
 
 const productsData = [
     {
