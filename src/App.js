d55f4340cdfd6b62373307554b8189afa0b9ee74
Your Name <you@example.com>
2021-01-19 21:58:42 +0400

    assets fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react'
+
 import Layout from './components/Layout/Layout';
 import Footer from './containers/Footer/Footer';
 import Header from './containers/Header/Header';
 import Products from './containers/Products/Products';
+import Info from './components/Info/Info';
 
 import './App.scss';
 
@@ -11,6 +14,7 @@ function App() {
       <Header />
       <Layout>
         <Products />
+        <Info />
       </Layout>
       <Footer />
     </div>
