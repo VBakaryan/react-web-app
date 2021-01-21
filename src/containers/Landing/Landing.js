147b2f87c71488d6a1a5f26cc181895ca29149ce
vahanbakaryan <vahan.bakaryan@peiapps.com>
2021-01-21 22:06:48 +0400

    Added landing container and styling

---
+++ src/containers/Landing/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Button from '../../components/Button/Button'
+import landingImg from '../../assets/landing/landing.png'
+
+import './Landing.scss'
+
+const Landing = () => {
+    return (
+        <div className="app-landing">
+            <div className="app-landing--img">
+                <img src={landingImg} alt="landing-img"></img>
+            </div>
+
+            <div className="app-landing--h1">
+                <h1 className="app-landing--text">
+                    Ապահովում ենք երջանկություն <br></br>և լավ տրամադրություն<br></br>
+                    բոլորին և ամենուր
+                </h1>
+                <Button className="app-landing--btn"> Ստանալ </Button>
+            </div>
+        </div>
+    )
+};
+
+export default Landing;
